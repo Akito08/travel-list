cfdb406d7cedd8ff6a23695e9375650af358525e
Akito08 <sza9ki8to7@gmail.com>
2023-11-02 11:42:51 +0900

    sort機能の追加

--- src/App.js
+++ src/App.js
@@ -77,10 +77,23 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "name")
+    sortedItems = items.slice().sort((a, b) => a.name.localeCompare(b.name));
+
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -89,6 +102,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div class="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="name">Sort by name</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
@@ -115,6 +135,7 @@ function Stats({ items }) {
         <em>Start adding some items to your packing list 🎃🎃</em>
       </p>
     );
+
   const numItems = items.length;
   const numPacked = items.filter((item) => item.packed).length;
   const percentage =
