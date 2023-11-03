ea112450b7e8135274d96b0f4d979194b9190be1
Akito08 <sza9ki8to7@gmail.com>
2023-11-03 12:22:59 +0900

    コンポーネントをfileごとに分割

---
+++ src/components/Form.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!name) return;
+
+    const newItem = { name, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setName("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 100 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button>ADD</button>
+    </form>
+  );
+}
